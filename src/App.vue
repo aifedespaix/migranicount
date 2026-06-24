8d23a9c07eecd84d31f8169a6a7f2a972a9f74b2
Aife <clapette01@gmail.com>
2026-06-24 14:33:36 +0200

    feat: add responsive navigation shell (bottom nav, header nav, FAB)

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,23 @@
 <template>
-  <RouterView />
+  <HeaderNav @add="formOpen = true" />
+  <main class="app-main">
+    <RouterView />
+  </main>
+  <BottomNav />
+  <FabButton @click="formOpen = true" />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import HeaderNav from './components/HeaderNav.vue'
+import BottomNav from './components/BottomNav.vue'
+import FabButton from './components/FabButton.vue'
+
+const formOpen = ref(false)
+</script>
+
+<style scoped>
+.app-main {
+  padding-bottom: 5rem;
+}
+</style>
